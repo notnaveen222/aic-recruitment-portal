874a86c3e09c31319be679969f7d98a643e0f76f
naveen <notnaveen2@gmail.com>
2025-10-12 12:24:47 +0530

    fixed build err

--- src/components/hero-badge.tsx
+++ src/components/hero-badge.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function HeroBadge() {
   return (
     <div className="bg-white/10 border border-white/30 px-3 py-px text-base rounded-full mb-3 w-fit mx-auto flex gap-x-3">
-      Best Technical Club'25 🏆
+      Best Technical Club&apos;25 🏆
     </div>
   );
 }
